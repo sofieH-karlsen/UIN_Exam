10b9f1e148c0749e4d74ef4441bf3525619b7f98
Karoline <kanojo94@gmail.com>
2022-04-29 13:21:31 +0200

    Bytting til 2.2, setup av Sanity, endring av navn på mapper

--- react_oppgave_2_2/frontend/src/index.js
+++ react_oppgave_2_2/frontend/src/index.js
@@ -8,7 +8,4 @@ import './styles/css/main.css'
 
 import App from './App'
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
