cf4d16710d8c0d5a252671dc5658f6a4b29ec790
Lars Helland <larshelland@Larss-MacBook-Pro.local>
2022-05-04 19:17:56 +0200

    scss

--- react_oppgave_2_2/frontend/src/components/Filter.js
+++ react_oppgave_2_2/frontend/src/components/Filter.js
@@ -1,23 +1,35 @@
-import { React, useState } from 'react'
+import { useState } from 'react'
 import { services } from '../data/services'
+import Card from './Card'
 
 export default function Filter() {
   const [searchTerm, setSearchTerm] = useState('')
   return (
+    <>
     <input
         data-testid="search"
         id="search"
         name="search"
         type="search"
         placeholder="Søk etter tjenester"
-        onChange={event => {setSearchTerm(event.target.value)}}
+        onChange={(event) => {setSearchTerm(event.target.value)}}
       />
-        (services.filter().map((val, key) => {
-            return (
-                <div className="service" key={key}>
-                <p>{val.id}</p>
-                </div>
-            )
-        })
-  )
+        {services.filter((service) => {
+          if (searchTerm == ""){
+            return service
+          } else if (service.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+
+          }
+        }).map((service) => (
+            
+              <Card
+              key={service.key}
+              title={service.title}
+              preamble={service.preAmble}
+              link={service.slug}
+            />
+          
+        ))}
+    </>
+  );
 }
