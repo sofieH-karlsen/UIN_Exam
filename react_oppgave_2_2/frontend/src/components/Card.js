5c099c6a3a22af2054cb642265488fb836a4cd19
Lars Helland <larshelland@Larss-MacBook-Pro.local>
2022-05-04 19:34:18 +0200

    scss

--- react_oppgave_2_2/frontend/src/components/Card.js
+++ react_oppgave_2_2/frontend/src/components/Card.js
@@ -1,12 +1,16 @@
 // import { NavLink } from 'react-router-dom'
+import '../styles/scss/main.scss'
 
 export default function Card({ key, title, preamble, link }) {
   return (
-    <article key={key}>
-      <h3>{title}</h3>
-      <p>{preamble}</p>
-      <a href={`tjenester/${link}`}>Til tjeneste</a>
-      {/* TODO REPLACE <NavLink path={`${link}`}>Til tjeneste</NavLink> */}
-    </article>
+    <div className="card">
+      <article key={key}>
+        <h3>{title}</h3>
+        <p>{preamble}</p>
+        <a href={`tjenester/${link}`}>Til tjeneste</a>
+        {/* TODO REPLACE <NavLink path={`${link}`}>Til tjeneste</NavLink> */}
+      </article>
+    </div>
+    
   )
 }
