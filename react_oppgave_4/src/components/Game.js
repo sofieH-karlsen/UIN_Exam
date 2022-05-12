505b845ff2f533994fd6a5e4c3a7e6529a89481c
Sofie Karlsen <92021448+Soso-the-Peach@users.noreply.github.com>
2022-05-12 23:46:32 +0200

    branch (oppg4) merge
    * update i package JSON
    
    * Start update
    
    * Kopiere filer
    
    * Endre import filsti
    
    * Fix Imports (mulig)
    
    * ææææææ
    
    * Lvl "nr" øker når knapp trykket
    
    * Pain
    
    * Console logs
    
    * ee
    
    * FUCK YOU *game* WE BEAT YOU
    
    * Total poeng skrevet
    
    * Only the if's left(i hope)
    
    * Rød Knappppppppppppppp
    
    * Ferdig(hopefully)
    
    * bare start.js igjen
    
    * ææææææææ
    
    * ......It's done
    
    * Ferdig
    
    Limer inn "kilder" og fjerner console.log()'s

---
+++ react_oppgave_4/src/components/Game.js
@@ -0,0 +1,59 @@
+// TODO: Nødvendig props
+import { useState, useEffect } from 'react'
+import BullsEye from './Bullseye'
+import Header from './Header'
+
+export default function Game({ game, nextLvl, setLvl }) {
+  // TODO: Legge til nødvendig logikk. Hvis nødvendig.
+  const [points, setPoints] = useState(0)
+
+  /* Inspo from: https://www.telerik.com/blogs/how-to-show-and-hide-elements-in-react */
+  const [hidden, setHidden] = useState(true)
+
+  const knapper = game.buttons
+  const maxPoint = game.total
+
+  const newLvl = () => {
+    nextLvl()
+    setPoints(0)
+  }
+  useEffect(() => {
+    if (points >= maxPoint) {
+      setHidden(false)
+    } else {
+      setHidden(true)
+    }
+  }, [maxPoint, points])
+
+  return (
+    <>
+      <Header max={maxPoint} points={points} />
+      <div data-testid="game" className="grid grid-cols-4 gap-6">
+        {/* TODO: Må skrive ut antall knapper et spill skal vise */}
+        {/* TODO: Må bruke riktig komponent og sende data som props */}
+        {knapper.map((button) => (
+          <BullsEye
+            color={button.color}
+            point={button.point}
+            setPoints={setPoints}
+            setLvl={setLvl}
+            open={hidden}
+          />
+        ))}
+        {/* TODO: Kun vise denne knappen når en runde er ferdig */}
+        <button
+          className={
+            hidden
+              ? 'hidden'
+              : 'col-span-4 w-full rounded-xl border-2 border-emerald-100 p-8 text-lg font-bold text-emerald-500 shadow-lg shadow-emerald-100'
+          }
+          type="button"
+          data-testid="next"
+          onClick={newLvl}
+        >
+          Neste runde
+        </button>
+      </div>
+    </>
+  )
+}
